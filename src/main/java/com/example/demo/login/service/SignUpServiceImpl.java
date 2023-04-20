917745b87bbe9197d690dcb0bc2817650ffcb64a
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-04-21 01:25:10 +0900

    feat: spring-security 의존 추가 및 비밀번호 암호화 기능 추가

--- src/main/java/com/example/demo/login/service/SignUpServiceImpl.java
+++ src/main/java/com/example/demo/login/service/SignUpServiceImpl.java
@@ -4,15 +4,19 @@ import com.example.demo.login.dto.UserDto;
 import com.example.demo.login.entity.User;
 import com.example.demo.login.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SignUpServiceImpl implements SignUpService{
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Long join(UserDto userDto) {
+        userDto.setPassword(passwordEncoder.encode(userDto.getPassword()));
+
         User user = User.builder()
                 .email(userDto.getEmail())
                 .username(userDto.getUsername())
