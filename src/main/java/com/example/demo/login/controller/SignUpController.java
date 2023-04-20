80f892d1aefba7c0989c9d45b3ac522df3df9403
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-04-20 21:05:50 +0900

    refactor: 회원가입 성공과 실패시 http 상태 코드 반환으로 변경

--- src/main/java/com/example/demo/login/controller/SignUpController.java
+++ src/main/java/com/example/demo/login/controller/SignUpController.java
@@ -3,11 +3,14 @@ package com.example.demo.login.controller;
 import com.example.demo.login.dto.UserDto;
 import com.example.demo.login.service.SignUpService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +19,10 @@ public class SignUpController {
     private final SignUpService signUpService;
 
     @PostMapping("/new")
-    public ResponseEntity signUp(@RequestBody UserDto userdto){
-        return ResponseEntity.ok(signUpService.join(userdto));
+    public ResponseEntity<Objects> signUp(@RequestBody UserDto userdto){
+        if(signUpService.join(userdto) > 0){
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 }
