20c3a2b9f451ba183d2a9af938749e42c6e2cccb
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-06-05 02:01:19 +0900

    refactor: 공공데이터key값 application.yml 숨겨 사용하도록 수정

--- src/main/java/com/example/demo/safetyplace/service/SafetyPlaceService.java
+++ src/main/java/com/example/demo/safetyplace/service/SafetyPlaceService.java
@@ -2,29 +2,27 @@ package com.example.demo.safetyplace.service;
 
 import com.example.demo.safetyplace.entity.SafetyPlace;
 import com.example.demo.safetyplace.repositroy.SafetyPlaceRepository;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.RequiredArgsConstructor;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
-import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class SafetyPlaceService {
     private final SafetyPlaceRepository safetyPlaceRepository;
     private final String apiUrl = "https://api.odcloud.kr/api/15002452/v1/uddi:4bc92095-bfa2-464f-8723-b65c6a336565";
-    //인증키는 따로 보관해서 안전하게 사용해야할듯
-    private final String serviceKey = "Infuser 9/gaTK+0+4IsgB43PG8bQtkz/UbkzJzw/N8PRL1ajzMOfsoa6l3zimjPL8d0ovIgoP9odKoqANhgzH0fgH5AfA==";
+    @Value("${config.secret}")
+    private String serviceKey;
     private final String parameter = "?page=0&perPage=0";
 
     public String findAll() throws ParseException {
