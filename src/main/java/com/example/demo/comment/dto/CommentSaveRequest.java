42dca90fb83baf2e00a40f2513786d311972a18c
Jongseok Kwon <jjijji5728@tukorea.ac.kr>
2023-07-11 15:34:29 +0900

    refactor: comment 패키지 내 dto -> record로 변환

--- src/main/java/com/example/demo/comment/dto/CommentSaveRequest.java
+++ src/main/java/com/example/demo/comment/dto/CommentSaveRequest.java
@@ -3,35 +3,9 @@ package com.example.demo.comment.dto;
 import com.example.demo.board.entity.Board;
 import com.example.demo.comment.entity.Comment;
 import com.example.demo.user.entity.User;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class CommentSaveRequest {
-    private String reply;
-    private User user;
-    private Board board;
-
-//    @Builder
-//    public CommentSaveRequest(String reply, User user, Board board){
-//        this.reply = reply;
-//        this.user = user;
-//        this.board = board;
-//    }
-
-//    public Comment toEntity(){
-//        return Comment.builder()
-//                .reply(reply)
-//                .user(user)
-//                .board(board)
-//                .build();
-//    }
-
-    public Comment toEntity(){
+public record CommentSaveRequest(String reply, User user, Board board) {
+    public Comment toEntity() {
         return new Comment(reply, user, board);
     }
 }
